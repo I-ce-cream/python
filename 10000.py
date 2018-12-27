8424a09f83454e88a0eb02c98d166d606b131a02
ZXZ <541459574@qq.com>
2018-12-27 17:43:33 +0800

    20181227

--- 10000.py
+++ 10000.py
@@ -1,20 +1,19 @@
-Prime = [2]
+Prime = [2,3,5,7]
 ret   = []
 
 def isPrime(num):
     for pri in Prime:
-        if num%pri == 0 :
-            return False
+        if num%pri == 0 :return False
     return True
 
 def Check(num):
-    for pri in Prime:
-        if (num - pri) in Prime:
-            return False
+    for i,pri in enumerate(Prime):
+        if (num - pri) in Prime: return False
+        if pri > (num/2) : return False
     return True
 
 def main(num):
-    for i in range(3,num + 1):
+    for i in range(10,num + 1):
         if isPrime(i):
             Prime.append(i)
         else:
@@ -27,4 +26,4 @@ def main(num):
 
 
 if __name__=='__main__':
-    main(100000)
+    main(10000)
